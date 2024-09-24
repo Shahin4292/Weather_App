d863bc937da882ebff93a49f12877f332080ff95
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-24 10:32:18 +0600

    weather service done

---
+++ lib/Services/services.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/Model/weather_model.dart';
+
+class WeatherService {
+  fetchWeather() async {
+    final response = await http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=28.5175&lon=81.7787&appid=509079b22fae7e954dff8403ef5eba0e"),
+    );
+    try {
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return WeatherData.fromJson(json);
+      }else{
+        throw Exception("Failed to load weather data");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
